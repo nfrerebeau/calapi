d9d4498d53ad807565b3d3b4b43d71ed5ab946cc
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2022-09-15 11:48:25 +0200

    Use memoise

--- plumber.R
+++ plumber.R
@@ -11,7 +11,10 @@
 #
 
 library(plumber)
+
 oxcAAR::setOxcalExecutablePath("OxCal/bin/OxCalLinux")
+
+memoise_oxcal_calibrate <- memoise::memoise(oxcAAR::oxcalCalibrate)
 
 #* @apiTitle 14C Calibration API
 
@@ -19,16 +22,27 @@ oxcAAR::setOxcalExecutablePath("OxCal/bin/OxCalLinux")
 #* @param names A `character` vector giving the names of the date (e.g. laboratory codes).
 #* @param dates A `numeric` vector giving the BP dates that should be calibrated.
 #* @param errors A `numeric` vector giving the standard deviation that should be calibrated
+#* @serializer print
 #* @get /oxcal/calibrate
 function(names, dates, errors){
-  rdates <- oxcAAR::oxcalCalibrate(as.numeric(dates), as.numeric(errors), names)
+  rdates <- memoise_oxcal_calibrate(as.numeric(dates), as.numeric(errors), names)
+  rdates
+}
+
+#* Calibrates a 14C Date Using OxCal
+#* @param names A `character` vector giving the names of the date (e.g. laboratory codes).
+#* @param dates A `numeric` vector giving the BP dates that should be calibrated.
+#* @param errors A `numeric` vector giving the standard deviation that should be calibrated
+#* @get /oxcal/calibrate/ranges
+function(names, dates, errors){
+  rdates <- memoise_oxcal_calibrate(as.numeric(dates), as.numeric(errors), names)
   oxcAAR::get_sigma_ranges(rdates)
 }
 
 #* Plot a 14C Date Using OxCal
 #* @serializer png
-#* @get /oxcal/plot
+#* @get /oxcal/calibrate/plot
 function(names, dates, errors){
-  rdates <- oxcAAR::oxcalCalibrate(as.numeric(dates), as.numeric(errors), names)
+  rdates <- memoise_oxcal_calibrate(as.numeric(dates), as.numeric(errors), names)
   plot(rdates)
 }
